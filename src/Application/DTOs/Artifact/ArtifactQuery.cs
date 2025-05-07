d7cc9388b53d7d674b5717e80a846b4dd1a41350
Nguyen Giang <giangnnt260703@gmail.com>
2025-05-07 14:54:56 +0700

    feat: implement artifact repository with CRUD operations and query functionality

---
+++ src/Application/DTOs/Artifact/ArtifactQuery.cs
@@ -0,0 +1,9 @@
+﻿using Application.DTOs.Pagination;
+
+namespace MuseTrip360.src.Application.DTOs.Artifact
+{
+    public class ArtifactQuery : PaginationReq
+    {
+        public string? SearchKeyword { get; set; }
+    }
+}
