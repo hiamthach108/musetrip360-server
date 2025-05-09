0382a94bb1b94cb8aa05c1535a9b25de8212332a
hiamthach108 <dreon1008@gmail.com>
2025-05-09 09:20:21 +0700

    chore: api userrole management

--- src/Application/Controllers/UserController.cs
+++ src/Application/Controllers/UserController.cs
@@ -1,6 +1,7 @@
 namespace MuseTrip360.Controllers;
 
 using Application.DTOs.User;
+using Application.DTOs.UserRole;
 using Application.Middlewares;
 using Application.Service;
 using Microsoft.AspNetCore.Mvc;
@@ -20,7 +21,6 @@ public class UserController : ControllerBase
   }
 
   [Protected]
-  [AdminOnly]
   [HttpGet("admin")]
   public async Task<IActionResult> Get([FromQuery] UserQuery query)
   {
@@ -30,7 +30,6 @@ public class UserController : ControllerBase
   }
 
   [Protected]
-  [AdminOnly]
   [HttpPost("admin")]
   public async Task<IActionResult> Create([FromBody] UserCreateDto req)
   {
@@ -40,7 +39,6 @@ public class UserController : ControllerBase
   }
 
   [Protected]
-  [AdminOnly]
   [HttpPut("admin/{id}")]
   public async Task<IActionResult> Update(Guid id, [FromBody] UserUpdateDto req)
   {
@@ -65,5 +63,50 @@ public class UserController : ControllerBase
     _logger.LogInformation("Update user profile request received");
 
     return await _service.HandleUpdateProfileAsync(req);
+  }
+
+  [Protected]
+  [HttpPut("profile/change-password")]
+  public async Task<IActionResult> ChangePassword([FromBody] ChangePassword req)
+  {
+    _logger.LogInformation("Change user password request received");
+
+    return await _service.HandleChangePassword(req);
+  }
+
+  [Protected]
+  [HttpGet("privileges")]
+  public async Task<IActionResult> GetUserRoles()
+  {
+    _logger.LogInformation("Get user privileges request received");
+
+    return await _service.HandleGetUserPrivileges();
+  }
+
+  [Protected]
+  [HttpGet("admin/{userId}/roles")]
+  public async Task<IActionResult> GetUserRoles(Guid userId)
+  {
+    _logger.LogInformation("Get user roles request received");
+
+    return await _service.HandleGetUserRoles(userId);
+  }
+
+  [Protected]
+  [HttpPost("roles")]
+  public async Task<IActionResult> AddUserRole([FromBody] UserRoleFormDto req)
+  {
+    _logger.LogInformation("Add user role request received");
+
+    return await _service.HandleAddUserRole(req);
+  }
+
+  [Protected]
+  [HttpDelete("roles")]
+  public async Task<IActionResult> DeleteUserRole([FromBody] UserRoleFormDto req)
+  {
+    _logger.LogInformation("Delete user role request received");
+
+    return await _service.HandleDeleteUserRole(req);
   }
 }
