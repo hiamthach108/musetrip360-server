281b8c9eb4ccc86b2c74abfa77349d7ba469d52b
Nguyen Giang <giangnnt260703@gmail.com>
2025-05-20 12:29:03 +0700

    feat(tour-online): comment swagger document

--- src/Application/Controllers/TourOnlineController.cs
+++ src/Application/Controllers/TourOnlineController.cs
@@ -25,6 +25,7 @@ public class TourOnlineController : ControllerBase
     /// <param name="query">Query parameters for filtering and pagination</param>
     /// <returns>A list of active online tours and total count</returns>
     /// <response code="200">Returns the list of online tours</response>
+    /// <response code="400">Invalid query parameters</response>
     [HttpGet]
     public async Task<IActionResult> GetAll([FromQuery] TourOnlineQuery query)
     {
@@ -37,6 +38,7 @@ public class TourOnlineController : ControllerBase
     /// <param name="id">The unique identifier of the online tour</param>
     /// <returns>The online tour if found</returns>
     /// <response code="200">Returns the requested online tour</response>
+    /// <response code="404">Online tour not found</response>
     [HttpGet("{id}")]
     public async Task<IActionResult> GetById([FromRoute] Guid id)
     {
@@ -49,21 +51,27 @@ public class TourOnlineController : ControllerBase
     /// <param name="museumId">The unique identifier of the museum</param>
     /// <returns>A list of online tours for the specified museum</returns>
     /// <response code="200">Returns the list of online tours</response>
+    /// <response code="401">Unauthorized - User is not authenticated</response>
+    /// <response code="403">Forbidden - User does not have admin privileges</response>
+    /// <response code="404">Museum not found</response>
     [Protected]
     [HttpGet("museums/{museumId}")]
-
     public async Task<IActionResult> GetByMuseumId([FromRoute] Guid museumId)
     {
         return await _adminTourOnlineService.GetAllByMuseumIdAsync(museumId);
     }
 
     /// <summary>
-    /// Create a new online tour
+    /// Create a new online tour for a museum
     /// </summary>
     /// <param name="museumId">The unique identifier of the museum to create the tour for</param>
     /// <param name="tourOnline">The online tour creation data</param>
     /// <returns>The created online tour</returns>
     /// <response code="201">Returns the newly created online tour</response>
+    /// <response code="400">Invalid input data</response>
+    /// <response code="401">Unauthorized - User is not authenticated</response>
+    /// <response code="403">Forbidden - User does not have admin privileges</response>
+    /// <response code="404">Museum not found</response>
     [Protected]
     [HttpPost("museums/{museumId}")]
     public async Task<IActionResult> Create([FromRoute] Guid museumId, [FromBody] TourOnlineCreateDto tourOnline)
@@ -78,6 +86,10 @@ public class TourOnlineController : ControllerBase
     /// <param name="tourOnline">The updated online tour data</param>
     /// <returns>The updated online tour</returns>
     /// <response code="200">Returns the updated online tour</response>
+    /// <response code="400">Invalid input data</response>
+    /// <response code="401">Unauthorized - User is not authenticated</response>
+    /// <response code="403">Forbidden - User does not have admin privileges</response>
+    /// <response code="404">Online tour not found</response>
     [Protected]
     [HttpPut("{id}")]
     public async Task<IActionResult> Update([FromRoute] Guid id, [FromBody] TourOnlineUpdateDto tourOnline)
@@ -90,6 +102,10 @@ public class TourOnlineController : ControllerBase
     /// </summary>
     /// <param name="id">The unique identifier of the online tour to delete</param>
     /// <returns>No content if successful</returns>
+    /// <response code="204">Online tour successfully deleted</response>
+    /// <response code="401">Unauthorized - User is not authenticated</response>
+    /// <response code="403">Forbidden - User does not have admin privileges</response>
+    /// <response code="404">Online tour not found</response>
     [Protected]
     [HttpDelete("{id}")]
     public async Task<IActionResult> Delete([FromRoute] Guid id)
@@ -103,6 +119,9 @@ public class TourOnlineController : ControllerBase
     /// <param name="id">The unique identifier of the online tour to activate</param>
     /// <returns>The activated online tour</returns>
     /// <response code="200">Returns the activated online tour</response>
+    /// <response code="401">Unauthorized - User is not authenticated</response>
+    /// <response code="403">Forbidden - User does not have admin privileges</response>
+    /// <response code="404">Online tour not found</response>
     [Protected]
     [HttpPatch("{id}/activate")]
     public async Task<IActionResult> Activate([FromRoute] Guid id)
@@ -116,6 +135,9 @@ public class TourOnlineController : ControllerBase
     /// <param name="id">The unique identifier of the online tour to deactivate</param>
     /// <returns>The deactivated online tour</returns>
     /// <response code="200">Returns the deactivated online tour</response>
+    /// <response code="401">Unauthorized - User is not authenticated</response>
+    /// <response code="403">Forbidden - User does not have admin privileges</response>
+    /// <response code="404">Online tour not found</response>
     [Protected]
     [HttpPatch("{id}/deactivate")]
     public async Task<IActionResult> Deactivate([FromRoute] Guid id)
@@ -126,9 +148,12 @@ public class TourOnlineController : ControllerBase
     /// <summary>
     /// Get a paginated list of all online tours (including inactive) for admin purposes
     /// </summary>
-    /// <param name="query">Query parameters for filtering and pagination</param>
+    /// <param name="query">Admin query parameters for filtering and pagination</param>
     /// <returns>A list of all online tours and total count</returns>
     /// <response code="200">Returns the list of online tours</response>
+    /// <response code="400">Invalid query parameters</response>
+    /// <response code="401">Unauthorized - User is not authenticated</response>
+    /// <response code="403">Forbidden - User does not have admin privileges</response>
     [Protected]
     [HttpGet("admin")]
     public async Task<IActionResult> GetAllAdmin([FromQuery] TourOnlineAdminQuery query)
