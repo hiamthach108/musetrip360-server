393fd6007ab9fbfb6abe498363464ba407592674
hiamthach108 <dreon1008@gmail.com>
2025-09-01 09:19:12 +0700

    update mailbox name

--- src/Core/Mail/Mail.cs
+++ src/Core/Mail/Mail.cs
@@ -27,7 +27,7 @@ public class MailService : IMailService
   public async Task SendEmailAsync(string email, string subject, string message)
   {
     var mimeMessage = new MimeMessage();
-    mimeMessage.From.Add(new MailboxAddress("Rescue & 360 Furniture", _smtpUsername));
+    mimeMessage.From.Add(new MailboxAddress("MuseTrip360", _smtpUsername));
     mimeMessage.To.Add(new MailboxAddress("Receiver Name", email));
 
     mimeMessage.Subject = subject;
