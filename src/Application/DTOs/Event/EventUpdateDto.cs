ab40abcc318d7864a82a4b1c78ff429c44603929
nguyengiang <giangnnt260703@gmail.com>
2025-09-01 10:06:57 +0700

    quick fix update event up

--- src/Application/DTOs/Event/EventUpdateDto.cs
+++ src/Application/DTOs/Event/EventUpdateDto.cs
@@ -2,7 +2,7 @@ using System.ComponentModel.DataAnnotations;
 using System.Text.Json;
 using Application.Shared.Enum;
 
-public class EventUpdateDto : IValidatableObject
+public class EventUpdateDto
 {
     [MaxLength(100)]
     public string? Title { get; set; }
@@ -14,18 +14,8 @@ public class EventUpdateDto : IValidatableObject
     [Range(0, int.MaxValue)]
     public int? Capacity { get; set; }
     [Range(0, int.MaxValue)]
-    public int? AvailableSlots { get; set; }
     public DateTime? BookingDeadline { get; set; }
     [Range(0, float.MaxValue)]
     public float? Price { get; set; }
     public JsonDocument? Metadata { get; set; }
-
-    public IEnumerable<ValidationResult> Validate(ValidationContext validationContext)
-    {
-        // Validate AvailableSlots against Capacity
-        if (AvailableSlots.HasValue && Capacity.HasValue && AvailableSlots.Value > Capacity.Value)
-        {
-            yield return new ValidationResult("Available slots cannot be greater than capacity");
-        }
-    }
 }
